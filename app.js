3a9c3e2c850729c6f69d121501ca9f2f99e59ca6
BakerShoeMaker <info@quackenworth.com>
2017-11-02 17:56:12 -0700

    Still working.

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
 var inquirer = require('inquirer');
 
-inquirer.prompt([/* Pass your questions in here */]).then(function (answers) {
-    // Use user feedback for... whatever!!
-});
+/*
+ // constructor function used to create programmers objects
+ function Programmer(name, position, age, language) {
+ this.name = name;
+ this.position = position;
+ this.age = age;
+ this.language = language;
+ }
 
-//Used to create an object representing the current word the user
-// is attempting to guess. This should contain word specific logic and data.
-function Word(){
-    
-    
-}
-//Used for each letter in the current word. Each letter object should either display an underlying character, or
-// a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This
-// should contain letter specific logic and data.
-function Letter() {
-    
+ // creates the printInfo method and applies it to all programmer objects
+ Programmer.prototype.printInfo = function() {
+ console.log("Name: " + this.name + "\nPosition: " + this.position + "\nAge: " +
+ this.age + "\nLanguages: " + this.language);
+ };
+*/
+
+/*Used to create an object representing the current word the user
+is attempting to guess. This should contain word specific logic and data.*/
+function Word(word){
+    this.word = word;
+
 }
 
-//You must keep track of the user's remaining guesses and prompt the user if they would like to end the game if none remain.
-//Each constructor function should be in it's own file and be exported and required wherever needed.
-//Look into function prototypes and use them for a few of your constructor's methods.
+Word.prototype.printInfo = function() {
+    console.log("Letter: " , this.word);
+};
+
+
+/*Used for each letter in the current word. Each letter object should either display an underlying character, or
+a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This
+should contain letter specific logic and data.*/
+function Letter(){
+
+}
+runInquire();
+
+function runInquire(){
+    inquirer.prompt([{
+            name: "letter",
+            message: "Guess a letter the word."
+        }]
+    ).then(function (answers) {
+        var inputOfUser = new Word(answers.letter);
+        inputOfUser.printInfo();
+        //console.log(answers.word);
+    });
+
+    //If all letters are guessed then stop the program.
+    //If all letters are NOT guessed then call again.
+    //runInquire();//keeps repeating the question.
+
+}
+
+
+
+/*You must keep track of the user's remaining guesses and prompt the user if they would
+like to end the game if none remain. Each constructor function should be in it's own file and be exported and
+required wherever needed. Look into function prototypes and use them for a few of your constructor's methods.*/
+
+
+
+
+
 
 
